cd6fece6fb3c246e06b4152866eb92e0e153057f
Nazar Kutsynyak <44039482+NazarKutsynyak@users.noreply.github.com>
2019-02-22 13:56:30 +0200

    Something

---
+++ API/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace API
+{
+    public class Class1 // Temporary name
+    {
+        public void SetState() // Fuction for sending information to client
+        {
+
+        }
+
+        public void MakeMove() // Function for getting information from client
+        {
+
+        }
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using API;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm started");
+            Class1 api = new Class1();
+            api.SetState();
+            Console.Read();
+        }
+    }
+}
