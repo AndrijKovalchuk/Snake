49d40579bd00e639b3ca020e250c4214ea5f47f4
Руслан Горлатий <Hrat1995@gmail.com>
2019-03-03 02:24:52 +0200

    Add enum Move

---
+++ Algorithm/Move.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    public enum Move
+    {
+        None = 0,
+        Up,
+        Down,
+        Right,
+        Left,        
+    }
+}
