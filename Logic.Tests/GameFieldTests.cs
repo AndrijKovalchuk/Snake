a85b14154d1f07ddea03acb341de190e308d2097
NazarKutsynyak <44039482+NazarKutsynyak@users.noreply.github.com>
2019-07-26 16:51:27 +0300

    Rename and restructure TestGameField, TestSnake

--- Logic.Tests/GameFieldTests.cs
+++ Logic.Tests/GameFieldTests.cs
@@ -1,32 +1,47 @@
-using NUnit.Framework;
-using Common;
-using Logic;
+
 
 namespace Tests
 {
-    public class TestGameField
+    using NUnit.Framework;
+    using Common;
+    using Logic;
+    
+    [TestFixture]
+    public class GameFieldTest
     {
         [SetUp]
         public void Setup()
-        {
+        {            
         }
 
         [Test]
-        public void TestMakeMove()
+        public void MakeMove_Left_Play()
         {
             GameField gameField = new GameField();
 
             gameField.MakeMove(Move.Left);
+
             Assert.That(gameField.Status, Is.EqualTo(GameStatus.Play));
-            
-            //Clear gameField
-            gameField = null;
-            gameField = new GameField();
+        }
+
+        [Test]
+        public void MakeMove_Right_Play()
+        {
+            GameField gameField = new GameField();
 
             gameField.MakeMove(Move.Right);
-            Assert.That(gameField.Status, Is.EqualTo(GameStatus.Play));
 
+            Assert.That(gameField.Status, Is.EqualTo(GameStatus.Play));
+        }
+
+        [Test]
+        public void MakeMove_RightThenLeft_GameOver()
+        {
+            GameField gameField = new GameField();
+
+            gameField.MakeMove(Move.Right);
             gameField.MakeMove(Move.Left);
+
             Assert.That(gameField.Status, Is.EqualTo(GameStatus.GameOver));
         }
     }
---
+++ Logic.Tests/SnakeTests.cs
@@ -0,0 +1,85 @@
+using NUnit.Framework;
+using Common;
+using Logic;
+
+namespace Tests
+{
+    public class SnakeTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void IsReverseMovement_UpThenDown_True()
+        {
+            Snake snake = new Snake(new Point(1, 1), Move.Up);
+
+            snake.SetMove(Move.Up);
+            snake.MoveIsEat(new Point(1, 10));
+            snake.SetMove(Move.Down);
+            
+            Assert.That(snake.IsReverseMovement(), Is.True);
+        }
+
+        [Test]
+        public void IsReverseMovement_UpThenRight_False()
+        {
+            Snake snake = new Snake(new Point(1, 1), Move.Up);
+
+            snake.SetMove(Move.Up);
+            snake.MoveIsEat(new Point(1, 10));
+            snake.SetMove(Move.Right);
+            
+            Assert.That(snake.IsReverseMovement(), Is.False);
+        }
+
+        [Test]
+        public void MoveIsEat_From1_1to1_2_True()
+        {
+            Snake snake = new Snake(new Point(1, 1), Move.Up);
+
+            snake.SetMove(Move.Up);
+            
+            Assert.That(snake.MoveIsEat(new Point(1, 2)), Is.True);
+        }
+
+        [Test]
+        public void MoveIsEat_From1_1to1_10_False()
+        {
+            Snake snake = new Snake(new Point(1, 1), Move.Up);
+
+            Assert.That(snake.MoveIsEat(new Point(1, 10)), Is.False);
+        }
+
+        [Test]
+        public void IsCrashed_Size4TurnRight4Times_False()
+        {
+            Snake snake = new Snake(new Point(1, 1), Move.Up);          
+
+            //Snake eat 3 times, she has size: 4
+            for(int i = 2; i < 5; i++)
+            {
+                snake.SetMove(Move.Up);
+                snake.MoveIsEat(new Point(1, i));
+            }
+
+            //Turn 4th times, on 4th time doesn't have to dead
+            snake.SetMove(Move.Right);
+            snake.MoveIsEat(new Point(1, 100));
+            snake.SetMove(Move.Down);
+            snake.MoveIsEat(new Point(1, 100));
+            snake.SetMove(Move.Left);
+            snake.MoveIsEat(new Point(1, 100));           
+            
+            TestContext.Out.WriteLine("Snake's coordinates:");
+            TestContext.Out.WriteLine(snake.Body[0].X + " " + snake.Body[0].Y);
+            TestContext.Out.WriteLine(snake.Body[1].X + " " + snake.Body[1].Y);
+            TestContext.Out.WriteLine(snake.Body[2].X + " " + snake.Body[2].Y);
+            TestContext.Out.WriteLine(snake.Body[3].X + " " + snake.Body[3].Y);
+
+            Assert.That(snake.IsCrashed(), Is.False);
+        }
+    }
+}
